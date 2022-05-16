85a151b1703687f4cea0b02cca0b7fe8d9452d1f
kiyo7 <kiyo324783@icloud.com>
2022-05-16 18:39:44 +0900

    News 表示

---
+++ src/components/Article/index.tsx
@@ -0,0 +1,38 @@
+import styles from './index.module.scss';
+import moment from 'moment';
+import { News } from '../../../types/types';
+
+const Article: React.FC<News> = ({ articles, title }) => {
+  return (
+    <section className={styles.article}>
+      <div className={styles.article__heading}>
+        <h1>{title.charAt(0).toUpperCase() + title.slice(1).toLowerCase()}</h1>
+      </div>
+      {articles.map((article, i) => {
+        const time = moment(article.publishedAt || moment.now())
+          .fromNow()
+          .slice(0, 1);
+        return (
+          <a href={article.url} key={i}>
+            <article className={styles.article__main}>
+              <div className={styles.article__title}>
+                <p>{article.title}</p>
+                <p className={styles.article__time}>{time}時間前</p>
+              </div>
+              {article.urlToImage && (
+                <img
+                  key={i}
+                  src={article.urlToImage}
+                  alt={`${article.title} image`}
+                  className={styles.article__img}
+                />
+              )}
+            </article>
+          </a>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Article;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { GetStaticProps } from 'next';
 import MainLayout from '../layouts';
 import Head from 'next/head';
 import { getAllNews } from '../../lib/fetch';
+import Article from '../components/Article';
+import styles from '../styles/Home.module.scss';
 
 export default function Home(props) {
   console.log(props.topArticles);
@@ -10,6 +12,9 @@ export default function Home(props) {
       <Head>
         <title>Simple News</title>
       </Head>
+      <div className={styles.main}>
+        <Article title="headlines" articles={props.topArticles} />
+      </div>
     </MainLayout>
   );
 }
