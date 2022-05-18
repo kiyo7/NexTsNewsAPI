a0dfc4eceea940c9e621840e166f8af511d66cff
kiyo7 <kiyo324783@icloud.com>
2022-05-18 11:30:06 +0900

    fin

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-require('dotenv').config();
 
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEWS_API_KEY: process.env.NEWS_API_KEY,
-    WEATHER_API_KEY: process.env.WEATHER_API_KEY,
-  },
 };
 
 module.exports = nextConfig;
